8de96b075d96a2ad8f9275f5e31628a2b66ca4ee
Lightningtow <alexwiest01@gmail.com>
2024-05-24 10:03:25 -0700

    Revert "init"
    This reverts commit b15e57aacc5d8019ae1cfe50611ff3ad26b9e59f.

---
+++ src/main/java/lightningtow/hudify/integrations/CustomhudIntegration.java
@@ -0,0 +1,69 @@
+package lightningtow.hudify.integrations;
+import com.minenash.customhud.HudElements.supplier.NumberSupplierElement;
+import com.minenash.customhud.HudElements.supplier.SpecialSupplierElement;
+import com.minenash.customhud.HudElements.supplier.StringSupplierElement;
+
+import static lightningtow.hudify.util.SpotifyData.*;
+
+
+/*? if =3.3 {*/
+import static com.minenash.customhud.mod_compat.CustomHudRegistry.registerElement;
+public class CustomhudIntegration {
+        public static void initCustomhud() {
+
+        UpdateMaps();
+        // don't put log messages here, already logged in HudifyMain
+        for ( String key : stringmap.keySet() ) {
+            registerElement(key, (_str) -> new StringSupplierElement(
+                    () -> stringmap.get(key).isEmpty() ? null : stringmap.get(key))); }
+
+        for ( String key : boolmap.keySet() ) {
+//            registerElement(key, (_str) -> new StringSupplierElement(() -> boolmap.get(key).toString())); }
+            registerElement(key, (_bool) -> new CustomhudBoolSupplier(() -> boolmap.get(key))); }
+
+        for ( String key : intmap.keySet() ) {
+            registerElement(key, (_int) -> new NumberSupplierElement(() -> intmap.get(key), 1)); }
+
+        for ( String key : specialmap.keySet() ) {
+            registerElement(key, (_special) -> new SpecialSupplierElement(SpecialSupplierElement.of(
+                    () -> specialmap.get(key).getA(),
+                    () -> specialmap.get(key).getB(),
+                    () -> specialmap.get(key).getC()
+            )));
+        }
+    }
+}
+/*?} */
+
+/*? if =4.0 {*//*
+import com.minenash.customhud.data.Flags;
+import com.minenash.customhud.HudElements.supplier.BooleanSupplierElement;
+import static com.minenash.customhud.data.Flags.wrap;
+import static com.minenash.customhud.registry.CustomHudRegistry.registerElement;
+public class CustomhudIntegration {
+        public static void initCustomhud() {
+
+            // don't put log messages here, messages already in HudifyMain
+            UpdateMaps();
+
+            for ( String key : stringmap.keySet() ) { // f, c == flags, context
+                registerElement(key, (f, c) ->
+                    wrap(new StringSupplierElement(() -> stringmap.get(key).isEmpty() ? null : stringmap.get(key)), f)); }
+
+            for ( String key : boolmap.keySet() ) {
+//                registerElement(key, (_bool) -> new BoolSupplier(() -> boolmap.get(key))); }
+                registerElement(key, (f, c) ->  wrap(new BooleanSupplierElement(() -> boolmap.get(key)), f)); }
+
+            for ( String key : intmap.keySet() ) {
+                registerElement(key, (f, c) -> new NumberSupplierElement(() -> intmap.get(key), new Flags())); }
+
+            for ( String key : specialmap.keySet() ) {
+                registerElement(key, (f, c) -> new SpecialSupplierElement(SpecialSupplierElement.of(
+                        () -> specialmap.get(key).getA(),
+                        () -> specialmap.get(key).getB(),
+                        () -> specialmap.get(key).getC()
+                )));
+            }
+        }
+}
+*//*?} */
---
+++ src/main/java/lightningtow/hudify/util/AuthServerHandler.java
@@ -0,0 +1,73 @@
+package lightningtow.hudify.util;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import lightningtow.hudify.HudifyMain;
+import org.apache.logging.log4j.Level;
+
+public class AuthServerHandler implements HttpHandler
+{
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException
+    {
+        String requestParamValue = null;
+        if ("GET".equals(httpExchange.getRequestMethod())) { requestParamValue = handleGetRequest(httpExchange); }
+        try {
+            handleResponse(httpExchange, requestParamValue);
+        } catch (URISyntaxException | InterruptedException e) {
+            HudifyMain.LogThis(Level.ERROR, e.getMessage());
+        }
+    }
+    private String handleGetRequest(HttpExchange httpExchange) {
+        return httpExchange.getRequestURI().toString()
+                .split("\\?")[1]
+                .split("=")[1];
+    }
+    private void handleResponse (HttpExchange httpExchange, String requestParamValue) throws IOException, URISyntaxException, InterruptedException
+    {
+        OutputStream outputStream = httpExchange.getResponseBody();
+        //        htmlBuilder.append("<html>").append("<body>").append("<h1>").append("Success!").append("</h1>").append("</body?").append("</html>");
+        //        String htmlResponse = htmlBuilder.toString();
+        String htmlResponse = "<html><body><h1>Success!</h1></body?</html>";
+        httpExchange.sendResponseHeaders(200, htmlResponse.length());
+        outputStream.write(htmlResponse.getBytes());
+        outputStream.flush();
+        outputStream.close();
+        lightningtow.hudify.util.SpotifyUtil.authorize(requestParamValue);
+    }
+
+
+    /**  PKCE UTILS */
+    public static String generateCodeVerifier()
+            throws UnsupportedEncodingException
+    {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[32];
+        secureRandom.nextBytes(codeVerifier);
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(
+                codeVerifier);
+    }
+
+
+    public static String generateCodeChallenge(String codeVerifier)
+            throws UnsupportedEncodingException,
+            NoSuchAlgorithmException
+    {
+        byte[] bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes, 0, bytes.length);
+        byte[] digest = messageDigest.digest();
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+    }
+}
