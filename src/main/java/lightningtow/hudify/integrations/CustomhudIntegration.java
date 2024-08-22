2f60c3117a29cd2231181dfddf90964df89dbb29
Lightningtow <alexwiest01@gmail.com>
2024-08-21 17:20:55 -0700

    1.20.4 build for customhud v4 beta

--- src/main/java/lightningtow/hudify/integrations/CustomhudIntegration.java
+++ src/main/java/lightningtow/hudify/integrations/CustomhudIntegration.java
@@ -1,56 +1,70 @@
 package lightningtow.hudify.integrations;
+import com.minenash.customhud.HudElements.supplier.BooleanSupplierElement;
 import com.minenash.customhud.HudElements.supplier.NumberSupplierElement;
 import com.minenash.customhud.HudElements.supplier.SpecialSupplierElement;
 import com.minenash.customhud.HudElements.supplier.StringSupplierElement;
 import static lightningtow.hudify.util.SpotifyData.*;
 
+/*
+TO SWITCH BETWEEN CUSTOMHUD VERSIONS:
+
+swap the dependencies in build.gradle
+swap customhud_api_label in gradle.properties
+comment the appropriate section below
+
+if switching to v4: comment the entire CustomhudBoolSupplier.java file
+if switching to v3: uncomment the entire CustomhudBoolSupplier.java file
+
+ */
+
+
 /* 4.0 */
-//import com.minenash.customhud.data.Flags;
-//import static com.minenash.customhud.data.Flags.wrap;
-//import static com.minenash.customhud.registry.CustomHudRegistry.registerElement;
-//public class CustomhudIntegration {
-//        public static void initCustomhud() {
-//            UpdateMaps();
-//            for ( String key : stringmap.keySet() ) { // f, c == flags, context
-//                registerElement(key, (f, c) ->  wrap(new StringSupplierElement(() -> stringmap.get(key).isEmpty() ? null : stringmap.get(key)), f)); }
-//
-//            for ( String key : boolmap.keySet() ) {
-//                registerElement(key, (f, c) ->  wrap(new BooleanSupplierElement(() -> boolmap.get(key)), f)); }
-//            for ( String key : intmap.keySet() ) {
-//                registerElement(key, (f, c) -> new NumberSupplierElement(() -> intmap.get(key), new Flags())); }
-//
-//            for ( String key : specialmap.keySet() ) {
-//                registerElement(key, (f, c) -> new SpecialSupplierElement(SpecialSupplierElement.of(
-//                        () -> specialmap.get(key).getA(),
-//                        () -> specialmap.get(key).getB(),
-//                        () -> specialmap.get(key).getC()
-//                )));
-//            }
-//        }
-//}
-
-
-/* 3.3 */
-import static com.minenash.customhud.mod_compat.CustomHudRegistry.registerElement;
+import com.minenash.customhud.data.Flags;
+import static com.minenash.customhud.data.Flags.wrap;
+import static com.minenash.customhud.registry.CustomHudRegistry.registerElement;
 public class CustomhudIntegration {
         public static void initCustomhud() {
-        UpdateMaps(); // don't put log messages here, messages already in HudifyMain
-        for ( String key : stringmap.keySet() ) {
-            registerElement(key, (_str) -> new StringSupplierElement(() -> stringmap.get(key).isEmpty() ? null : stringmap.get(key))); }
+            UpdateMaps();
+            for ( String key : stringmap.keySet() ) { // f, c == flags, context
+                registerElement(key, (f, c) ->  wrap(new StringSupplierElement(() -> stringmap.get(key).isEmpty() ? null : stringmap.get(key)), f)); }
 
-        for ( String key : boolmap.keySet() ) {
-//            registerElement(key, (_str) -> new StringSupplierElement(() -> boolmap.get(key).toString())); }
-            registerElement(key, (_bool) -> new CustomhudBoolSupplier(() -> boolmap.get(key))); }
+            for ( String key : boolmap.keySet() ) {
+                registerElement(key, (f, c) ->  wrap(new BooleanSupplierElement(() -> boolmap.get(key)), f)); }
+            for ( String key : intmap.keySet() ) {
+                registerElement(key, (f, c) -> new NumberSupplierElement(() -> intmap.get(key), new Flags())); }
 
-        for ( String key : intmap.keySet() ) {
-            registerElement(key, (_int) -> new NumberSupplierElement(() -> intmap.get(key), 1)); }
-
-        for ( String key : specialmap.keySet() ) {
-            registerElement(key, (_special) -> new SpecialSupplierElement(SpecialSupplierElement.of(
-                    () -> specialmap.get(key).getA(),
-                    () -> specialmap.get(key).getB(),
-                    () -> specialmap.get(key).getC()
-            )));
+            for ( String key : specialmap.keySet() ) {
+                registerElement(key, (f, c) -> new SpecialSupplierElement(SpecialSupplierElement.of(
+                        () -> specialmap.get(key).getA(),
+                        () -> specialmap.get(key).getB(),
+                        () -> specialmap.get(key).getC()
+                )));
+            }
         }
-    }
 }
+
+
+///* 3.3 */
+//import static com.minenash.customhud.mod_compat.CustomHudRegistry.registerElement;
+//public class CustomhudIntegration {
+//        public static void initCustomhud() {
+//        UpdateMaps(); // don't put log messages here, messages already in HudifyMain
+//        for ( String key : stringmap.keySet() ) {
+//            registerElement(key, (_str) -> new StringSupplierElement(() -> stringmap.get(key).isEmpty() ? null : stringmap.get(key))); }
+//
+//        for ( String key : boolmap.keySet() ) {
+//            registerElement(key, (_str) -> new StringSupplierElement(() -> boolmap.get(key).toString())); } // for v4
+////            registerElement(key, (_bool) -> new CustomhudBoolSupplier(() -> boolmap.get(key))); } // for v3
+//
+//        for ( String key : intmap.keySet() ) {
+//            registerElement(key, (_int) -> new NumberSupplierElement(() -> intmap.get(key), 1)); }
+//
+//        for ( String key : specialmap.keySet() ) {
+//            registerElement(key, (_special) -> new SpecialSupplierElement(SpecialSupplierElement.of(
+//                    () -> specialmap.get(key).getA(),
+//                    () -> specialmap.get(key).getB(),
+//                    () -> specialmap.get(key).getC()
+//            )));
+//        }
+//    }
+//}
