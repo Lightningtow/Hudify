58152fbc2f02e832596bf0a234c2d8f97f386118
Lightningtow <alexwiest01@gmail.com>
2024-08-28 06:22:50 -0700

    removed midnightlib jar-in-jar
    made it so people have to fetch their own midnight lib

--- src/main/java/lightningtow/hudify/integrations/CustomhudIntegration.java
+++ src/main/java/lightningtow/hudify/integrations/CustomhudIntegration.java
@@ -5,44 +5,7 @@ import com.minenash.customhud.HudElements.supplier.SpecialSupplierElement;
 import com.minenash.customhud.HudElements.supplier.StringSupplierElement;
 import static lightningtow.hudify.util.SpotifyData.*;
 
-/*
-TO SWITCH BETWEEN CUSTOMHUD VERSIONS:
-
-swap the dependencies in build.gradle
-swap customhud_api_label in gradle.properties
-comment the appropriate section below
-
-if switching to v4: comment the entire CustomhudBoolSupplier.java file
-if switching to v3: uncomment the entire CustomhudBoolSupplier.java file
-
- */
 // MAIN BRANCH SHOULD NOT EVER BE USING 4.0 STUFF
-
-/* 4.0 */
-//import com.minenash.customhud.data.Flags;
-//import static com.minenash.customhud.data.Flags.wrap;
-//import static com.minenash.customhud.registry.CustomHudRegistry.registerElement;
-//public class CustomhudIntegration {
-//        public static void initCustomhud() {
-//            UpdateMaps();
-//            for ( String key : stringmap.keySet() ) { // f, c == flags, context
-//                registerElement(key, (f, c) ->  wrap(new StringSupplierElement(() -> stringmap.get(key).isEmpty() ? null : stringmap.get(key)), f)); }
-//
-//            for ( String key : boolmap.keySet() ) {
-//                registerElement(key, (f, c) ->  wrap(new BooleanSupplierElement(() -> boolmap.get(key)), f)); }
-//            for ( String key : intmap.keySet() ) {
-//                registerElement(key, (f, c) -> new NumberSupplierElement(() -> intmap.get(key), new Flags())); }
-//
-//            for ( String key : specialmap.keySet() ) {
-//                registerElement(key, (f, c) -> new SpecialSupplierElement(SpecialSupplierElement.of(
-//                        () -> specialmap.get(key).getA(),
-//                        () -> specialmap.get(key).getB(),
-//                        () -> specialmap.get(key).getC()
-//                )));
-//            }
-//        }
-//}
-
 
 ///* 3.3 */
 import static com.minenash.customhud.mod_compat.CustomHudRegistry.registerElement;
