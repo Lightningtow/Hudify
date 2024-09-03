465fa08ebaf11bc86b854976e30aac7be910788c
Lightningtow <alexwiest01@gmail.com>
2024-09-02 22:09:31 -0700

    updated version number
    also cleaned up albumart json, no more triple duplicated horror string

--- src/main/java/lightningtow/hudify/HudifyMain.java
+++ src/main/java/lightningtow/hudify/HudifyMain.java
@@ -284,6 +284,8 @@ public class HudifyMain implements ClientModInitializer
 
 	}
 
+	public static final int jsonImageChoice = 1; // 0 == 640, 1 == 300, 2 == 64
+
 	public static void updatePlaybackInfo()
 	{
 		String dump_msg = "getPlaybackInfo";
@@ -329,17 +331,19 @@ public class HudifyMain implements ClientModInitializer
 //				sp_track = json.get("item").getAsJsonObject().get("name").getAsString();
 //				sp_fancy_track = HudifyConfig.scrub_name ? SpotifyUtil.scrub(sp_track) : sp_track;
 
+				String albumart = json.get("item").getAsJsonObject().get("album").getAsJsonObject().get("images").getAsJsonArray().get(jsonImageChoice).getAsJsonObject().get("url").getAsString();
+
 //				sp_icon_link = json.get("item").getAsJsonObject().get("album").getAsJsonObject().get("images").toString();
 //[20:24:01] [Spotify Thread/INFO] (Hudify) (Hudify) [{"height":640,"url":"https://i.scdn.co/image/ab67616d0000b273cfc4b1939aba562fc97159c5","width":640},{"height":300,"url":"https://i.scdn.co/image/ab67616d00001e02cfc4b1939aba562fc97159c5","width":300},{"height":64,"url":"https://i.scdn.co/image/ab67616d00004851cfc4b1939aba562fc97159c5","width":64}]
 				//sp_icon_link = json.get("item").getAsJsonObject().get("album").getAsJsonObject().get("images").getAsJsonArray().get(0).toString();
 // [20:27:10] [Spotify Thread/INFO] (Hudify) (Hudify) {"height":640,"url":"https://i.scdn.co/image/ab67616d0000b2733b4362147e2b0595d512033e","width":640}
-				sp_album_art_link = json.get("item").getAsJsonObject().get("album").getAsJsonObject().get("images").getAsJsonArray().get(2).getAsJsonObject().get("url").getAsString();
+				sp_album_art_link = albumart;
 
-				if (sp_prev_album_art_link.isEmpty() || !sp_prev_album_art_link.equals( json.get("item").getAsJsonObject().get("album").getAsJsonObject().get("images").getAsJsonArray().get(2).getAsJsonObject().get("url").getAsString())) {
+				if (sp_prev_album_art_link.isEmpty() || !sp_prev_album_art_link.equals(albumart)) {
 					// if album art changed or is empty
                     LogThis(Level.INFO,"album art links do NOT match, running getAlbumArt");
 
-					sp_prev_album_art_link = json.get("item").getAsJsonObject().get("album").getAsJsonObject().get("images").getAsJsonArray().get(2).getAsJsonObject().get("url").getAsString();
+					sp_prev_album_art_link = albumart;
 					getAlbumArt();
 					LogThis(Level.INFO, "got album art");
 
